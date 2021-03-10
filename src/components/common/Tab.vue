70d2b7129892b5743d2f8389e8cfa9e8242bcfee
Task Ohmori <tascii.oh@gmail.com>
2021-03-10 18:53:27 +0900

    Tailwind migration of dapps page basic

---
+++ src/components/common/Tab.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav class="ml-12 -mb-px flex justify-items-center" aria-label="Tabs">
+        <router-link
+            append
+            v-for="num in labelsNumArray"
+            :to="{ path: labels[num].path }"
+            :key="num"
+            v-on:click="change(num)"
+            v-bind:class="[active === num ? activeLinkClass : inactiveLinkClass]"
+        >
+            <span v-bind:class="[active === num ? activeSpanClass : inactiveSpanClass]">
+                {{ labels[num].label }}
+            </span>
+        </router-link>
+    </nav>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType, reactive, ref } from 'vue';
+
+export default defineComponent({
+    props: {
+        labels: { type: Array as PropType<Array<{ label: string; path: string }>>, required: true },
+    },
+
+    setup(props) {
+        console.log(props.labels[0].path);
+        const active = ref(0);
+        const change = (num: number) => {
+            active.value = num;
+        };
+        const classes = reactive({
+            activeLinkClass: 'border-gray-200 dark:border-darkGray-600 border rounded-t-md',
+            inactiveLinkClass: 'border-gray-50 dark:border-darkGray-900 border rounded-t-md',
+            activeSpanClass:
+                'block bg-gray-50 dark:bg-darkGray-900 -mb-px whitespace-nowrap py-3 sm:py-5 px-8 text-blue-900 dark:text-darkGray-300 font-medium rounded-t-md border-gray-50 dark:border-darkGray-900 border-b',
+            inactiveSpanClass:
+                'block bg-gray-50 dark:bg-darkGray-900 -mb-px whitespace-nowrap py-3 sm:py-5 px-8 text-blue-500 dark:text-blue-400 font-medium rounded-t-md border-gray-200 dark:border-darkGray-600 border-b hover:text-blue-400 dark:hover:text-blue-300',
+        });
+        const labelsNumArray = computed(() => [...Array(props.labels.length).keys()]);
+        return {
+            labelsNumArray,
+            active,
+            change,
+            ...props,
+            ...classes,
+        };
+    },
+});
+</script>
