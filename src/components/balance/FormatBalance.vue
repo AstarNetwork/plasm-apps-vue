b06bc4d2f4d94d8a41f09c20e3e43ebe8a58ed40
Will Park <korea651@hanmail.net>
2021-04-19 16:20:47 +0900

    set dynamic decimal for balance

--- src/components/balance/FormatBalance.vue
+++ src/components/balance/FormatBalance.vue
@@ -1,24 +1,27 @@
 <template>
-  <div>{{ formatBalance }} PLM</div>
+  <div>{{ formatBalance }} {{ unitToken }}</div>
 </template>
 <script lang="ts">
-//https://github.com/polkadot-js/apps/blob/86284f815500d9edd25228db603a18983e46878d/packages/react-query/src/FormatBalance.tsx#L34
 import { defineComponent, inject, computed, ref } from 'vue';
-// import { useApi } from '@/hooks';
+import { useApi } from '@/hooks';
 import BN from 'bn.js';
 import * as plasmUtils from '@/helper';
-import { Registry } from '@polkadot/types/types';
 
 export default defineComponent({
   setup() {
-    // const { api } = useApi();
-    // getFormat(api.registry, formatIndex)
+    const { api } = useApi();
+
+    const registry = api?.value?.registry;
+
+    const decimals = registry?.chainDecimals;
+    const tokens = registry?.chainTokens;
+    const decimal = (decimals || [])[0];
+    const unitToken = (tokens || [])[0];
 
     const balance = inject('balance', ref(new BN(0)));
 
     const formatBalance = computed(() => {
-      // FIXME: the tokenDecimal value is the current default for Plasm mainnet. We should dynamically parse this from the chain.
-      const tokenDecimal = 10;
+      const tokenDecimal = decimal;
       return plasmUtils.reduceBalanceToDenom(
         balance.value.clone(),
         tokenDecimal
@@ -27,18 +30,9 @@ export default defineComponent({
 
     return {
       formatBalance,
+      unitToken,
     };
   },
-  methods: {
-    getFormat(registry: Registry, formatIndex = 0): [number, string] {
-      const decimals = registry.chainDecimals;
-      const tokens = registry.chainTokens;
-
-      return [
-        formatIndex < decimals.length ? decimals[formatIndex] : decimals[0],
-        formatIndex < tokens.length ? tokens[formatIndex] : tokens[1],
-      ];
-    },
-  },
+  methods: {},
 });
 </script>
