eec643dc78ecf1fe8bfe0d12a2c9d514b57e3299
leo <leounoki22@gmail.com>
2021-06-16 16:44:24 +0900

    add helper in param, add index field to MessageType

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -2,6 +2,7 @@ import type { TypeDef } from '@polkadot/types/create/types';
 import type { AbiParam } from '@polkadot/api-contract/types';
 
 export interface MessageType {
+  index: number;
   identifier: string;
   docs: string[];
   args: AbiParam[];
