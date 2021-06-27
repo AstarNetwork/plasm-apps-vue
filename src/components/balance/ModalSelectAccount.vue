1fc2b0f83fd18c49d3e8b84de64a0db85f64a6c7
Will Park <korea651@hanmail.net>
2021-06-28 02:27:51 +0900

    linting

--- src/components/balance/ModalSelectAccount.vue
+++ src/components/balance/ModalSelectAccount.vue
@@ -3,39 +3,12 @@
     <button
       type="button"
       @click="openOption = !openOption"
-      class="
-        relative
-        text-blue-900
-        dark:text-darkGray-100
-        w-full
-        bg-white
-        dark:bg-darkGray-900
-        border border-gray-300
-        dark:border-darkGray-500
-        rounded-md
-        pl-3
-        pr-10
-        py-3
-        text-left
-        focus:outline-none
-        focus:ring focus:ring-blue-100
-        dark:focus:ring-darkGray-600
-        hover:bg-gray-50
-        dark:hover:bg-darkGray-800
-      "
+      class="relative text-blue-900 dark:text-darkGray-100 w-full bg-white dark:bg-darkGray-900 border border-gray-300 dark:border-darkGray-500 rounded-md pl-3 pr-10 py-3 text-left focus:outline-none focus:ring focus:ring-blue-100 dark:focus:ring-darkGray-600 hover:bg-gray-50 dark:hover:bg-darkGray-800"
     >
       <div class="flex items-center justify-between">
         <div class="flex items-center">
           <div
-            class="
-              h-8
-              w-8
-              rounded-full
-              overflow-hidden
-              border border-gray-100
-              mr-3
-              flex-shrink-0
-            "
+            class="h-8 w-8 rounded-full overflow-hidden border border-gray-100 mr-3 flex-shrink-0"
           >
             <icon-base class="h-full w-full" viewBox="0 0 64 64">
               <icon-account-sample />
@@ -43,16 +16,7 @@
           </div>
 
           <input
-            class="
-              w-full
-              text-blue-900
-              dark:text-darkGray-100
-              text-xl
-              focus:outline-none
-              bg-transparent
-              placeholder-gray-300
-              dark:placeholder-darkGray-600
-            "
+            class="w-full text-blue-900 dark:text-darkGray-100 text-xl focus:outline-none bg-transparent placeholder-gray-300 dark:placeholder-darkGray-600"
             style="width: 21rem"
             type="text"
             spellcheck="false"
@@ -62,16 +26,7 @@
       </div>
 
       <span
-        class="
-          ml-3
-          absolute
-          inset-y-0
-          right-0
-          flex
-          items-center
-          pr-2
-          pointer-events-none
-        "
+        class="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
       >
         <icon-base
           class="h-5 w-5 text-gray-400 dark:text-darkGray-300"
@@ -86,29 +41,10 @@
 
     <div
       v-if="openOption"
-      class="
-        block
-        absolute
-        mt-1
-        w-full
-        rounded-md
-        bg-white
-        dark:bg-darkGray-800
-        shadow-lg
-        z-10
-        border border-gray-200
-        dark:border-darkGray-600
-      "
+      class="block absolute mt-1 w-full rounded-md bg-white dark:bg-darkGray-800 shadow-lg z-10 border border-gray-200 dark:border-darkGray-600"
     >
       <ul
-        class="
-          max-h-56
-          rounded-md
-          py-1
-          text-base
-          overflow-auto
-          focus:outline-none
-        "
+        class="max-h-56 rounded-md py-1 text-base overflow-auto focus:outline-none"
       >
         <!-- Select option -->
         <ModalSelectAccountOption
--- src/components/common/ModalAlertBox.vue
+++ src/components/common/ModalAlertBox.vue
@@ -9,48 +9,17 @@
       </div>
 
       <div
-        class="
-          inline-block
-          bg-white
-          dark:bg-darkGray-900
-          rounded-lg
-          px-4
-          sm:px-8
-          py-10
-          overflow-hidden
-          shadow-xl
-          transform
-          transition-all
-          mx-2
-          my-2
-          align-middle
-          max-w-lg
-          w-full
-        "
+        class="inline-block bg-white dark:bg-darkGray-900 rounded-lg px-4 sm:px-8 py-10 overflow-hidden shadow-xl transform transition-all mx-2 my-2 align-middle max-w-lg w-full"
       >
         <div>
           <div>
             <h3
-              class="
-                text-lg
-                font-extrabold
-                text-blue-900
-                dark:text-white
-                mb-6
-                text-center
-              "
+              class="text-lg font-extrabold text-blue-900 dark:text-white mb-6 text-center"
             >
               Alert
             </h3>
             <div
-              class="
-                mt-1
-                w-full
-                rounded-md
-                bg-white
-                dark:bg-darkGray-900
-                dark:text-white
-              "
+              class="mt-1 w-full rounded-md bg-white dark:bg-darkGray-900 dark:text-white"
             >
               {{ msg }}
             </div>
@@ -60,27 +29,7 @@
           <button
             type="button"
             @click="closeModal"
-            class="
-              inline-flex
-              items-center
-              px-6
-              py-3
-              border border-gray-300
-              dark:border-darkGray-500
-              text-sm
-              font-medium
-              rounded-full
-              text-gray-500
-              dark:text-darkGray-400
-              bg-white
-              dark:bg-darkGray-900
-              hover:bg-gray-100
-              dark:hover:bg-darkGray-700
-              focus:outline-none
-              focus:ring focus:ring-gray-100
-              dark:focus:ring-darkGray-600
-              mx-1
-            "
+            class="inline-flex items-center px-6 py-3 border border-gray-300 dark:border-darkGray-500 text-sm font-medium rounded-full text-gray-500 dark:text-darkGray-400 bg-white dark:bg-darkGray-900 hover:bg-gray-100 dark:hover:bg-darkGray-700 focus:outline-none focus:ring focus:ring-gray-100 dark:focus:ring-darkGray-600 mx-1"
           >
             Close
           </button>
